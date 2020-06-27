20428bd95454409176c888c6ca07b7e2088eb11f
Syed Ali <i.syedali@hotmail.com>
2020-06-27 06:17:45 +0300

    server.js was edited

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http= require('http');
 function onRequest(request, response) {
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Hello Ali');
+	response.write('Hello Syed Ali');
 	response.end();
 }
 
