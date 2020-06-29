5744eb322f6ebc257f1689e2aee432f98bdb4a34
i-syedali <i.syedali@users.noreply.github.com>
2020-06-29 10:42:18 +0300

    server.js was edited

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http= require('http');
 function onRequest(request, response) {
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Hello Syed Ali');
+	response.write('Welcome Syed Ali');
 	response.end();
 }
 
